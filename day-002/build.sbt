8cd469d49f22886b65900691ac58a96cdd46bae4
tcabel <tristan.cabel@amadeus.com>
2017-01-09 12:06:17 +0100

    merge 2

--- day-002/build.sbt
+++ day-002/build.sbt
@@ -2,6 +2,8 @@ name := "recursion"
 
 version := "1.0"
 
-scalaVersion := "2.11.5"
+scalaVersion := "2.12.1" // This verison can be changed according to each environment, we are not using features by version yet
 
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.1" % "test"
+
+
--- day-002/src/test/scala/recursionSpec.scala
+++ day-002/src/test/scala/recursionSpec.scala
@@ -1,72 +1,103 @@
 import leo.training._
 
-import org.scalatest._
+import org.scalatest.FlatSpec
 
-class RecursionSpec extends FlatSpec with Matchers{
-  "factImp with 1" should "be 1" in {
-    val res = recursion.factImp(1)
-    res should be (1)
+class RecursionSpec extends FlatSpec {
+
+    //TODO make this with test generators or other method, as this way is too repetitive (this can be done in other non copy and paste ways)
+  "factImp" should "be 0" in {
+    assert(recursion.factImp(0) === 1) //the triple equal === method is given by scalatest, it will print out nthe values that are failing in case of error
   }
 
-  "factImp with 3" should "be 6" in {
-    recursion.factImp(3) should be (6)
+  "factImp" should "be 1" in {
+    assert(recursion.factImp(1) === 1)
+  }
+  "factImp" should "be 2" in {
+    assert(recursion.factImp(2) === 2)
+  }
+  "factImp" should "be 6" in {
+    assert(recursion.factImp(3) === 6)
+  }
+  "factImp" should "be 24" in {
+    assert(recursion.factImp(4) === 24)
+  }
+  "factImp" should "be 120" in {
+    assert(recursion.factImp(5) === 120)
+  }
+  "factImp" should "be 720" in {
+    assert(recursion.factImp(6) === 720)
+  }
+  "factImp of a negative number" should "be refused" in {
+    try{
+      recursion.factImp(-1)
+    }
+    catch {
+      case _:IllegalArgumentException => //Expected
+    }
+    succeed
   }
 
-  "factRec with 1" should "be 1" in {
-    val res = recursion.factRec(1)
-    res should be (1)
+  "factRec" should "be 0" in {
+    assert(recursion.factRec(0) === 1)
+  }
+  "factRec" should "be 1" in {
+    assert(recursion.factRec(1) === 1)
+  }
+  "factRec" should "be 2" in {
+    assert(recursion.factRec(2) === 2)
+  }
+  "factRec" should "be 6" in {
+    assert(recursion.factRec(3) === 6)
+  }
+  "factRec" should "be 24" in {
+    assert(recursion.factRec(4) === 24)
+  }
+  "factRec" should "be 120" in {
+    assert(recursion.factRec(5) === 120)
+  }
+  "factRec" should "be 720" in {
+    assert(recursion.factRec(6) === 720)
+  }
+  "factRec of a negative number" should "be refused" in {
+    try{
+      recursion.factRec(-1)
+    }
+    catch {
+      case _:IllegalArgumentException => //Expected
+    }
+    succeed
+  }
+  "factTailRec" should "be 0" in {
+    assert(recursion.factTailRec(0) === 1)
+  }
+  "factTailRec" should "be 1" in {
+    assert(recursion.factTailRec(1) === 1)
+  }
+  "factTailRec" should "be 2" in {
+    assert(recursion.factTailRec(2) === 2)
+  }
+  "factTailRec" should "be 6" in {
+    assert(recursion.factTailRec(3) === 6)
+  }
+  "factTailRec" should "be 24" in {
+    assert(recursion.factTailRec(4) === 24)
+  }
+  "factTailRec" should "be 120" in {
+    assert(recursion.factTailRec(5) === 120)
+  }
+  "factTailRec" should "be 720" in {
+    assert(recursion.factTailRec(6) === 720)
+  }
+  "factTailRec of a negative number" should "be refused" in {
+    try{
+      recursion.factTailRec(-1)
+    }
+    catch {
+      case _:IllegalArgumentException => //Expected
+    }
+    succeed
   }
 
-  "factRec with 3" should "be 6" in {
-    recursion.factRec(3) should be (6)
-  }
 
-  "fibImp with 1" should "be 1" in {
-    recursion.fibImp(1) should be (1)
-  }
-
-  "fibImp with 2" should "be 1" in {
-    recursion.fibImp(2) should be (1)
-  }
-
-  "fibImp with 3" should "be 2" in {
-    recursion.fibImp(3) should be (2)
-  }
-
-  "fibImp with 4" should "be 3" in {
-    recursion.fibImp(4) should be (3)
-  }
-
-  "fibImp with 5" should "be 5" in {
-    recursion.fibImp(5) should be (5)
-  }
-
-  "fibImp with 6" should "be 8" in {
-    recursion.fibImp(6) should be (8)
-  }
-
-
-  "fibRec with 1" should "be 1" in {
-    recursion.fibRec(1) should be (1)
-  }
-
-  "fibRec with 2" should "be 1" in {
-    recursion.fibRec(2) should be (1)
-  }
-
-  "fibRec with 3" should "be 2" in {
-    recursion.fibRec(3) should be (2)
-  }
-
-  "fibRec with 4" should "be 3" in {
-    recursion.fibRec(4) should be (3)
-  }
-
-  "fibRec with 5" should "be 5" in {
-    recursion.fibRec(5) should be (5)
-  }
-
-  "fibRec with 6" should "be 8" in {
-    recursion.fibRec(6) should be (8)
-  }
+  //TODO Exercise: Make the tests for the Fibonacci implementation
 }
